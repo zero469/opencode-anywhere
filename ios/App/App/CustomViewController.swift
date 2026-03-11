648989df894303a99d7125d0539e797f3a70084a
Yao Liu <liuyao4133@qq.com>
2026-03-11 17:05:05 +0800

    Fix CustomViewController - move config to viewDidAppear

--- ios/App/App/CustomViewController.swift
+++ ios/App/App/CustomViewController.swift
@@ -4,14 +4,11 @@ import Capacitor
 class CustomViewController: CAPBridgeViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        webView?.scrollView.contentInsetAdjustmentBehavior = .never
-        webView?.backgroundColor = .clear
-        webView?.isOpaque = false
-        webView?.scrollView.backgroundColor = .clear
     }
     
-    override var preferredStatusBarStyle: UIStatusBarStyle {
-        return .lightContent
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        webView?.scrollView.contentInsetAdjustmentBehavior = .never
     }
 }
